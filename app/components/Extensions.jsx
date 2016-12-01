8656a3b231bc1efa0d68518699494b7e3b3ef0da
Leann Wong <leann@leann-MacBook-Pro.local>
2016-12-01 14:06:25 +0800

    Rename example page

--- app/components/Extensions.jsx
+++ app/components/Extensions.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Extensions = (props) => {
 	return (
 		<div>
-			<h3>Extensions Component!</h3>	
+			<h3>Extensions</h3>	
 			<p>Welcome to extensions page!</p>
 		</div>
 	)
